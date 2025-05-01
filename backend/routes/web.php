7463644814daf43721148e078a1eabc942666438
Dmitrii <dmitry.beresnev@icloud.com>
2025-05-01 11:53:26 +0300

    Correct name and structure routs

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -12,27 +12,37 @@ Route::get('/', function () {
 });
 //Клиентская часть
 Route::get('/dashboard', [DashboardController::class,"view"]);
-
 Route::get('/{name}', [SubjectController::class,"view"]);
+
 
 //Админская чать
 //Только залогиненные могут иметь возможность работать с темами и тестами
 Route::get('/admin', [AdminController::class,"view"]);
 
-Route::get('admin/create_topic', [TopicController::class,"create"]);
-Route::get('admin/update_topic', [TopicController::class,"update"]);
-Route::get('admin/delete_topic', [TopicController::class,"delete"]);
 
-Route::get('admin/create_test', [TestController::class,"create"]);
-Route::get('admin/update_test', [TestController::class,"update"]);
-Route::get('admin/delete_test', [TestController::class,"delete"]);
+Route::get('admin/topic', [TopicController::class,"index"]);                //Вывод всех тем клиента
+Route::get('admin/topic/all', [TopicController::class,"show"]);             //Вывод всех тем
+Route::get('admin/topic/{id}', [TopicController::class,"view"]);            //Вывод конкретной темы
 
-Route::get('admin/view_topic', [TopicController::class,"index"]);
-Route::get('admin/view_topic/{id}', [TopicController::class,"view"]);
+Route::get('admin/topic/create', [TopicController::class,"create"]);        //Форма создания темы
+Route::get('admin/topic/edit', [TopicController::class,"edit"]);            //Форма изменения темы
+Route::post('admin/topic/{id}/store', [TopicController::class,"store"]);    //Запись БД
+Route::post('admin/topic/{id}/update', [TopicController::class,"update"]);  //Изменения БД
+Route::get('admin/topic/{id}/delete', [TopicController::class,"delete"]);   //Удаление БД
+
+
+Route::get('admin/test', [TestController::class,"index"]);                  //Вывод всех тестов клиенту
+Route::get('admin/test/create', [TestController::class,"create"]);          //Форма создания теста
+Route::get('admin/test/edit', [TestController::class,"edit"]);              //Форма изменения теста
+Route::post('admin/test/{id}/store', [TestController::class,"store"]);      //Запись БД
+Route::post('admin/test/{id}/update', [TestController::class,"update"]);    //Изменения БД
+Route::get('admin/test/{id}/delete', [TestController::class,"delete"]);     //Удаление БД
+
 
 //Только администратор должен иметь возможность создавать предметы/разделы
-Route::get('admin/create_subject', [SubjectController::class,"create"]);
-Route::get('admin/update_subject', [SubjectController::class,"update"]);
-Route::get('admin/delete_subject', [SubjectController::class,"delete"]);
-
-Route::get('admin/view_subject', [SubjectController::class,"view"]);
+Route::get('admin/subject', [SubjectController::class,"index"]);                  //Вывод всех тестов клиенту
+Route::get('admin/subject/create', [SubjectController::class,"create"]);          //Форма создания теста
+Route::get('admin/subject/edit', [SubjectController::class,"edit"]);              //Форма изменения теста
+Route::post('admin/subject/{id}/store', [SubjectController::class,"store"]);      //Запись БД
+Route::post('admin/subject/{id}/update', [SubjectController::class,"update"]);    //Изменения БД
+Route::get('admin/subject/{id}/delete', [SubjectController::class,"delete"]);     //Удаление БД
