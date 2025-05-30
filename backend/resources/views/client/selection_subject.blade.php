fa59dd5c4068960e71a060d3843ab6240d24f79e
Dmitrii <dmitry.beresnev@icloud.com>
2025-05-30 08:16:32 +0300

    secure_asset select subject

--- backend/resources/views/client/selection_subject.blade.php
+++ backend/resources/views/client/selection_subject.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $subject->name }} | Обучение и тестирование</title>
-    <link rel="icon" href="{{ asset('assets/icon/user-graduate-solid.svg') }}" type="image/x-icon">
+    <link rel="icon" href="{{ secure_asset('assets/icon/user-graduate-solid.svg') }}" type="image/x-icon">
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <style>
@@ -327,7 +327,7 @@
 
 
      <!-- JQUERY JS -->
-    <script src="{{ asset('assets/js/jquery.min.js') }}"></script>
+    <script src="{{ secure_asset('assets/js/jquery.min.js') }}"></script>
     <script>
         
         // Current test state
